b0075b474a708712d478fd367053ea12420a5488
Soraya Valle <svalleortega@gmail.com>
2020-06-02 13:31:18 +0200

    Add styling to details and link

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 'use strict';
 
-window.addEventListener('keydown', (ev) => {
+const playSound = (ev) => {
   const audio = document.querySelector(`audio[data-key="${ev.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${ev.keyCode}"]`);
   if (!audio) return; // stops the function if there's not audio associated with the key pressed.
   audio.currentTime = 0; // rewind to the start to 'force' key sounding whenever pressed even if its sound's still playing. 
   audio.play();
   key.classList.add('playing');
-});
+};
 
 function removeTransition (ev) {
   if (ev.propertyName !== 'transform') return; // skip property if it's not a transform.
@@ -16,3 +16,5 @@ function removeTransition (ev) {
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
