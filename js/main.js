f24ba57e123db2b7507506439376fe7fe7b2172b
Soraya Valle <svalleortega@gmail.com>
2020-06-02 12:34:42 +0200

    Create audio function and start styling

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 'use strict';
 
-window.addEventListener('keydown', function(ev) {
-
+window.addEventListener('keydown', (ev) => {
+  const audio = document.querySelector(`audio[data-key="${ev.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${ev.keyCode}"]`);
+  console.log(key)
+  if (!audio) return; // stops the function if there's not audio associated with the key pressed.
+  audio.currentTime = 0; // rewind to the start to 'force' key sounding whenever pressed even if its sound's still playing. 
+  audio.play();
 });
