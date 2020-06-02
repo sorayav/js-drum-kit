fc07b331de2d18f6e4030d21a4281cef9bbde2fc
Soraya Valle <svalleortega@gmail.com>
2020-06-02 12:56:23 +0200

    Create function to remove class to individual keys

--- js/main.js
+++ js/main.js
@@ -3,8 +3,16 @@
 window.addEventListener('keydown', (ev) => {
   const audio = document.querySelector(`audio[data-key="${ev.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${ev.keyCode}"]`);
-  console.log(key)
   if (!audio) return; // stops the function if there's not audio associated with the key pressed.
   audio.currentTime = 0; // rewind to the start to 'force' key sounding whenever pressed even if its sound's still playing. 
   audio.play();
+  key.classList.add('playing');
 });
+
+function removeTransition (ev) {
+  if (ev.propertyName !== 'transform') return; // skip property if it's not a transform.
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
